675791c34b19cc94358f87a5f35afeedfb451e3d
William <will.henshaw@eatstreet.com>
2019-07-11 13:31:43 -0500

    import data from json files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const users = require('./data/users.json');
+const tickets = require('./data/tickets.json');
+const organizations = require('./data/organizations.json');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
